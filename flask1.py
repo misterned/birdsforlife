ef35f71aaba693ebee3e1ab7f155c63ffc8f259a
misterned <nedelec.stephane@yahoo.fr>
2022-11-30 10:52:04 +0100

    Ajout du csv

---
+++ flask1.py
@@ -0,0 +1,31 @@
+import flask
+
+app = flask.Flask(__name__)
+
+app.config["DEBUG"] = True
+@app.route('/', methods=['GET'])
+
+books = [
+{'id': 0,
+'title': 'A Fire Upon the Deep',
+'author': 'Vernor Vinge',
+'first_sentence': 'The coldsleep itself was dreamless.',
+'year_published': '1992'},
+    
+{'id': 1,
+'title': 'The Ones Who Walk Away From Omelas',
+'author': 'Ursula K. Le Guin',
+'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the
+city Omelas, bright-towered by the sea.',
+'published': '1973'},
+    
+{'id': 2,
+'title': 'Dhalgren',
+'author': 'Samuel R. Delany',
+'first_sentence': 'to wound the autumnal city.',
+'published': '1975'}
+]
+
+def home():
+    return "<h1>Distant Reading Archive</h1>"
+app.run()
