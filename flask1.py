c4b2c93bbaa2d1c7a0f55f59593d3f6d7c7ddc2b
misterned <nedelec.stephane@yahoo.fr>
2022-12-05 10:59:52 +0100

    ajout de test conditionnel

--- flask1.py
+++ flask1.py
@@ -7,8 +7,10 @@ app.config["DEBUG"] = True
 list_latin = ['Vanellus vanelli', 'Ardea cinerea', 'Carduelis carduelis']
 tuple_francais = ('Vanneau huppé', 'Héron Cendré', 'Chardonneret élégant')
 
+
 @app.route("/")
 def home():
     return render_template("home.html", message = "le dernier oiseau "
-                           +"que j'ai photographié est un "+tuple_francais[-1])
+                           +"que j'ai photographié est un "
+                           + tuple_francais[-1], nb = len(tuple_francais))
 app.run()
