ee738c5b9af90278bbce0edd3c0fcba5f63d90cb
Mini mac stephane nedelec <stephanenedelec@mac-mini-1.home>
2022-12-06 06:07:06 +0100

    flask et les deux pages

--- flask2.py
+++ flask2.py
@@ -1,12 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template("home.html", message = "Hello World!")
+list_latin = ['Vanellus vanelli', 'Ardea cinerea', 'Carduelis carduelis']
+tuple_francais = ('Vanneau huppé', 'Héron Cendré', 'Chardonneret élégant')
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/")
+def home():
+    return render_template("home.html", message = "le dernier oiseau "
+                           +"que j'ai photographié est un "
+                           + tuple_francais[-1], nb = len(tuple_francais),
+                           franc = tuple_francais)
+
+@app.route("/next")
+def suite():
+    return render_template("page_suivante.html",nb = len(tuple_francais))
+
+app.run()
